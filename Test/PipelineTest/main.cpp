0906d6e2f1ebf06d85b5adacaa0c4ec03eefcb51
Zhu Shuang <breakersol@outlook.com>
2023-10-19 18:02:51 +0800

    Start demo test
    Start demo test.

--- Test/PipelineTest/main.cpp
+++ Test/PipelineTest/main.cpp
@@ -2,16 +2,11 @@
 #include "MetaTest.h"
 #include "Components/TA_ThreadPool.h"
 
+#include "Cu/TestCuda.cu"
+
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc,argv);
 
     return RUN_ALL_TESTS();
-
-//    static CoreAsync::TA_ThreadPool theadPool;
-//    std::function<void()> func = []() {std::printf("123456");};
-//    auto activity = CoreAsync::ITA_ActivityCreator::create(func);
-//    theadPool.postActivity(activity);
-
-    return 0;
 }
